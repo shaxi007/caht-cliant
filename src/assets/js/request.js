c1070703329be62c8407bcdd92e85c198add8ea3
Teshayev Shaxboz <teshayevshaxboz77@gmail.com>
2021-08-13 15:10:27 +0500

    updated

--- src/assets/js/request.js
+++ src/assets/js/request.js
@@ -1,4 +1,4 @@
-let host = 'http://localhost:8080'
+let host = 'https://chatt-sh.herokuapp.com/'
 async function request(path, method, body , token) { 
 	let headers = {
 			'Content-Type': 'application/json'
